8b1bb1b4ea878f971589bf40f049cd09f07023a8
DoctorTone <tony@drt-software.com>
2024-06-07 16:10:51 +0100

    Alter text on circular menu

--- src/UI/CircularOptionsPortfolioMenu.jsx
+++ src/UI/CircularOptionsPortfolioMenu.jsx
@@ -109,7 +109,7 @@ const CircularOptionsMainMenu = ({ onToggle, open }) => {
         </CircleMenuItem>
       </CircleMenu>
       <Button variant="dark" className="mt-1 orangeText" size="sm">
-        Options
+        Demos
       </Button>
     </div>
   );
